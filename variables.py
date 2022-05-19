2675532bc947b6b75d26527a1d9aea07e3e74cc9
yimjohns <yimjohns.dev@gmail.com>
2022-05-19 23:49:30 +0100

    Add Rules for Naming Variables to variables.py file

--- variables.py
+++ variables.py
@@ -7,4 +7,11 @@ case = "Upper"
 
 print(case + letter)
 
+# Rules for naming variables
+
+# A variable name must start with a letter or the underscore character.
+# A variable name cannot start with a number.
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+
 
