0b69bbe1eb54db5436c8421512b42d5e08f73c8f
yimjohns <yimjohns.dev@gmail.com>
2022-07-05 21:46:33 +0100

    pass in python

---
+++ intro.py
@@ -0,0 +1,24 @@
+from msilib.schema import Class
+
+
+def add(number1, number2):
+    pass
+
+def multiply(number1, number2):
+    pass
+
+def divide(number1, number2):
+    ...
+
+def subtract(number1, number2):
+    pass # I will implement this later
+
+class EmptyClass:
+    pass # Do nothing with the class but syntactically make it available
+
+
+def main():
+    print("Program entry point")
+
+if __name__ == "__main__":
+    main()
